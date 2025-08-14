49d11db384a14fc28938c5a49f0d0fd508e4abb8
Pamela Dias <dzindzkdiaspamela@gmail.com>
2025-08-14 11:24:03 -0300

    atividade de hj

---
+++ for.c.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void main(){
+    char nome [100] = "Pâmela";
+    
+    /*printf("%c\n", nome[0]);
+    printf("%c\n", nome[1]);
+    printf("%c\n", nome[2]);
+    printf("%c\n", nome[3]);
+    printf("%c\n", nome[4]);
+    printf("%c\n", nome[5]);
+    printf("%c\n", nome[6]);
+    printf("%c\n", nome[7]);
+    printf("%c\n", nome[8]);
+    
+    for(/*VARIAVEL DE CONTROLE*/;
+    /*CONDICIONAL DE CONTROLE*/
+    /*CONTADOR DA VARAVEL DE CONTROLE*/
+    
+    for(int i = 0; i<= 8; i = i +1){
+    printf("%d\n", i);
+    printf("%c\m",nome[i]);
+    }
+    
+    
+}
+    
