2ac52157c91d03e68f5a9615aed1e47e059c313d
Pamela Dias <dzindzkdiaspamela@gmail.com>
2025-09-05 11:18:38 -0300

    aula dia 05/09

---
+++ funcoes.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main(){
+ printf("Bom dia, sextou com S de saudade!\n");
+ int A = 0;
+ int B = 0;
+ 
+ printf("Digite o valor de A:");
+ scanf("%d", &A);
+ 
+ printf("Digite o valor de B:");
+ scanf("%d", &B);
+ 
+ 
+ printf("A soma de A + B = %d", A+B);
+ 
+}
